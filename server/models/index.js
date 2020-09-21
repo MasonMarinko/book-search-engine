cd291818dd54848911411c02234a36e53bc49461
MasonMarinko <mason.p.marinko@gmail.com>
2020-09-21 16:37:02 -0600

    finished, working on readme

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,3 @@
-const router = require('express').Router();
-const userRoutes = require('./user-routes');
+const User = require('./User');
 
-router.use('/users', userRoutes);
-
-module.exports = router;
+module.exports = { User };
