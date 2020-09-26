ef9e1b42bb714264232ec661c96074f44b455e9f
MasonM <Mason.P.Marinko@gmail.com>
2020-09-26 14:14:25 -0600

    heroku PLEASE

---
+++ client/build/precache-manifest.05f968e36adde83f4f689755ed06e053.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d2fc3f55f2ed70e42209c2302cc167f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0819c889f34dc2a5197d",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "c6c77513073407617407",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "0819c889f34dc2a5197d",
+    "url": "/static/js/2.372871b2.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/static/js/2.372871b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6c77513073407617407",
+    "url": "/static/js/main.92e2c721.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  }
+]);
