5aaaa40c2f61ed0faa854f185ae73d67ab1afcf4
MasonM <Mason.P.Marinko@gmail.com>
2020-09-26 13:29:54 -0600

    heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://crason8:masonhi@cluster0.pqqbc.mongodb.net/deepthoughts?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
