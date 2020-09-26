fb1e7352ed712b4bd2a1fc9184a2718b4f3dc586
MasonM <Mason.P.Marinko@gmail.com>
2020-09-26 15:18:57 -0600

    heroku

--- server/server.js
+++ server/server.js
@@ -24,12 +24,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Serve up static assets
-if(process.env.NODE_ENV === 'production'){
-  const path  =  require('path');
-  app.get('/*',(req,res)=>{
-      res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-  })
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
+
+app.use(express.static("public"))
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
